bc365e116613359ff396104ea17b8b102343d536
cuongnb14 <cuongnb14@gmail.com>
2015-09-16 19:42:35 +0700

    init

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/shophoaqua/admin/');
+define('HTTP_CATALOG', 'http://localhost/shophoaqua/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/shophoaqua/admin/');
+define('HTTPS_CATALOG', 'http://localhost/shophoaqua/');
+
+// DIR
+define('DIR_APPLICATION', '/opt/lampp/htdocs/shophoaqua/admin/');
+define('DIR_SYSTEM', '/opt/lampp/htdocs/shophoaqua/system/');
+define('DIR_DATABASE', '/opt/lampp/htdocs/shophoaqua/system/database/');
+define('DIR_LANGUAGE', '/opt/lampp/htdocs/shophoaqua/admin/language/');
+define('DIR_TEMPLATE', '/opt/lampp/htdocs/shophoaqua/admin/view/template/');
+define('DIR_CONFIG', '/opt/lampp/htdocs/shophoaqua/system/config/');
+define('DIR_IMAGE', '/opt/lampp/htdocs/shophoaqua/image/');
+define('DIR_CACHE', '/opt/lampp/htdocs/shophoaqua/system/cache/');
+define('DIR_DOWNLOAD', '/opt/lampp/htdocs/shophoaqua/download/');
+define('DIR_LOGS', '/opt/lampp/htdocs/shophoaqua/system/logs/');
+define('DIR_CATALOG', '/opt/lampp/htdocs/shophoaqua/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'shophoaqua');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/shophoaqua/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/shophoaqua/');
+
+// DIR
+define('DIR_APPLICATION', '/opt/lampp/htdocs/shophoaqua/catalog/');
+define('DIR_SYSTEM', '/opt/lampp/htdocs/shophoaqua/system/');
+define('DIR_DATABASE', '/opt/lampp/htdocs/shophoaqua/system/database/');
+define('DIR_LANGUAGE', '/opt/lampp/htdocs/shophoaqua/catalog/language/');
+define('DIR_TEMPLATE', '/opt/lampp/htdocs/shophoaqua/catalog/view/theme/');
+define('DIR_CONFIG', '/opt/lampp/htdocs/shophoaqua/system/config/');
+define('DIR_IMAGE', '/opt/lampp/htdocs/shophoaqua/image/');
+define('DIR_CACHE', '/opt/lampp/htdocs/shophoaqua/system/cache/');
+define('DIR_DOWNLOAD', '/opt/lampp/htdocs/shophoaqua/download/');
+define('DIR_LOGS', '/opt/lampp/htdocs/shophoaqua/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'shophoaqua');
+define('DB_PREFIX', 'oc_');
+?>
